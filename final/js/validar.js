e60a42f8ba5f9147452c25a703632e5ebc14e3a4
Santiago <boris96bs7@gmail.com>
2019-06-24 14:04:29 -0500

    nueva funcion validar - editar
    nueva funcion validar - editar

--- final/js/validar.js
+++ final/js/validar.js
@@ -62,3 +62,70 @@ if (!expRegCarga.exec(carga) || (carga>0 || carga>12))
      }
 }
 }
+
+
+//********************** VALIDAR (EDITAR) USUARIOS ******************************
+function validarEditarUsuarios()
+{
+//validarcedula
+ var i;
+ var cedula;
+ var acumulado;
+ var cedula=document.getElementById('cedula').value;
+ var instancia;
+ acumulado=0;
+ for (i=1;i<=9;i++)
+ {
+  if (i%2!=0)
+  {
+   instancia=cedula.substring(i-1,i)*2;
+   if (instancia>9) instancia-=9;
+  }
+  else instancia=cedula.substring(i-1,i);
+  acumulado+=parseInt(instancia);
+ }
+ while (acumulado>0)
+  acumulado-=10;
+ if (cedula.substring(9,10)!=(acumulado*-1))
+ {
+  alert("La cédula ingresada no es correcta");
+  return false;
+ }
+//validarnombre
+var expRegNombre=/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var nombre=document.getElementById('nombreu').value;
+if (!expRegNombre.exec(nombre))
+     {
+        alert("El nombre sólo contiene letras y un espacio");
+        return false;
+     }
+//validarapellido
+var expRegApellido=/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var apellido=document.getElementById('apellidou').value;
+if (!expRegApellido.exec(apellido))
+     {
+        alert("El apellido sólo contiene letras y un espacio");
+        return false;
+     }
+//validartelefono
+var expRegTelefono=/^[0-9]*$/;
+var telefono=document.getElementById('telefonou').value;
+if(telefono.length!=""){
+if (!expRegTelefono.exec(telefono) || telefono.length<9)
+     {
+        alert("El número de teléfono ingresado no es correcto");
+        return false;
+     }
+}
+
+//validarcarga
+var expRegCarga=/^[0-9]*$/;
+var carga=document.getElementById('cargau').value;
+if(carga.length!=""){
+if (!expRegCarga.exec(carga) || (carga>0 || carga>12))
+     {
+        alert("La carga horaria ingresada no es correcta");
+        return false;
+     }
+}
+}
