7a3d4110590a41300b60bc3155e72797dcfe0577
Santiago <boris96bs7@gmail.com>
2019-06-25 01:00:37 -0500

    validaacion emprresa
    validacion empresa

--- final/js/validar.js
+++ final/js/validar.js
@@ -44,7 +44,7 @@ if (!expRegApellido.exec(apellido))
 var expRegTelefono=/^[0-9]*$/;
 var telefono=document.getElementById('telefono').value;
 if(telefono.length!=""){
-if (!expRegTelefono.exec(telefono) || telefono.length<9)
+if (!expRegTelefono.exec(telefono) || telefono.length<9 || telefono.length>10)
      {
         alert("El número de teléfono ingresado no es correcto");
         return false;
@@ -121,7 +121,7 @@ if (!expRegApellido.exec(apellido))
 var expRegTelefono=/^[0-9]*$/;
 var telefono=document.getElementById('telefonou').value;
 if(telefono.length!=""){
-if (!expRegTelefono.exec(telefono) || telefono.length<9)
+if (!expRegTelefono.exec(telefono) || telefono.length<9 || telefono.length>10)
      {
         alert("El número de teléfono ingresado no es correcto");
         return false;
@@ -206,4 +206,154 @@ if (!expRegnombre.exec(nombre))
        alert("El nombre sólo contiene letras y un espacio");
        return false;
     }
+}
+
+
+//********************** VALIDAR EMPRESA ******************************
+function validarEmpresa()
+{
+var codigo, nombreEmpresa, siglas, ciudad, paginaweb, telefono, descripcion;
+
+//validarCodigo
+var expRegcodigo=/^[0-9]*$/;
+var codigo=document.getElementById('codigo').value;
+if(codigo.length!=""){
+if (!expRegcodigo.exec(codigo) || codigo<0)
+     {
+        alert("El codigo ingresado solo admite números");
+        return false;
+     }
+}
+
+//validarNombreEmporesa
+var expRegnombreEmpresa=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var nombreEmpresa=document.getElementById('nombre').value;
+if (!expRegnombreEmpresa.exec(nombreEmpresa))
+     {
+        alert("El nombre no es correcta");
+        return false;
+     }
+
+//validarSiglas
+var expRegsiglas=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var siglas=document.getElementById('siglas').value;
+   if (!expRegsiglas.exec(siglas))
+        {
+           alert("La siglas no estan correctas");
+           return false;
+        }
+
+//validarCiudad
+var expRegciudad=/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var ciudad=document.getElementById('ciudad').value;
+  if (!expRegciudad.exec(ciudad))
+       {
+          alert("La cidad no estan correctas");
+          return false;
+       }
+
+//validarPaginaWeb
+var expRegcpaginaweb=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ.]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ.]+)*$/;
+var paginaweb=document.getElementById('PaginaWeb').value;
+     if (!expRegpaginaweb.exec(paginaweb))
+          {
+             alert("La pagina web no esta correcta");
+             return false;
+          }
+
+//validartelefono
+var expRegtelefono=/^[0-9]*$/;
+var telefono=document.getElementById('telefono').value;
+  if(telefono.length!=""){
+  if (!expRegtelefono.exec(telefono) || telefono.length<9 || telefono.length>10)
+     {
+        alert("El número de teléfono ingresado no es correcto");
+        return false;
+     }
+}
+
+//validarDescripcion
+var expRegdescripcion=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var descripcion=document.getElementById('descripcion').value;
+  if (!expRegdescripcion.exec(descripcion))
+       {
+          alert("La descripcion no es correcta");
+          return false;
+       }
+
+}
+
+
+//********************** VALIDAR (EDITAR) EMPRESA ******************************
+function validarEditarEmpresa()
+{
+var codigo, nombreEmpresa, siglas, ciudad, paginaweb, telefono, descripcion;
+
+//validarCodigo
+var expRegcodigo=/^[0-9]*$/;
+var codigo=document.getElementById('codigou').value;
+if(codigo.length!=""){
+if (!expRegcodigo.exec(codigo) || codigo<0)
+     {
+        alert("El codigo ingresado solo admite números");
+        return false;
+     }
+}
+
+//validarNombreEmporesa
+var expRegnombreEmpresa=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var nombreEmpresa=document.getElementById('nombreu').value;
+if (!expRegnombreEmpresa.exec(nombreEmpresa))
+     {
+        alert("El nombre no es correcta");
+        return false;
+     }
+
+//validarSiglas
+var expRegsiglas=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var siglas=document.getElementById('siglasu').value;
+   if (!expRegsiglas.exec(siglas))
+        {
+           alert("La siglas no estan correctas");
+           return false;
+        }
+
+//validarCiudad
+var expRegciudad=/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var ciudad=document.getElementById('ciudadu').value;
+  if (!expRegciudad.exec(ciudad))
+       {
+          alert("La cidad no estan correctas");
+          return false;
+       }
+
+//validarPaginaWeb
+var expRegcpaginaweb=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ.]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ.]+)*$/;
+var paginaweb=document.getElementById('paginawebu').value;
+     if (!expRegpaginaweb.exec(paginaweb))
+          {
+             alert("La pagina web no esta correcta");
+             return false;
+          }
+
+//validartelefono
+var expRegtelefono=/^[0-9]*$/;
+var telefono=document.getElementById('telefonou').value;
+  if(telefono.length!=""){
+  if (!expRegtelefono.exec(telefono) || telefono.length<9 || telefono.length>10)
+     {
+        alert("El número de teléfono ingresado no es correcto");
+        return false;
+     }
+}
+
+//validarDescripcion
+var expRegdescripcion=/^[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [0-9a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+var descripcion=document.getElementById('descripcionu').value;
+  if (!expRegdescripcion.exec(descripcion))
+       {
+          alert("La descripcion no es correcta");
+          return false;
+       }
+
 }
