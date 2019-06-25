e2a13a7912c267ea0f90d08267ac07dec7fc49a9
Santiago <boris96bs7@gmail.com>
2019-06-24 23:37:03 -0500

    ingresar empreesa
    ingresar empresa

--- final/php/ingresarRepreEmpre.php
+++ final/php/ingresarRepreEmpre.php
@@ -1,14 +1,14 @@
 <?php
 
 //uso el include para insertar codigo de otro archivo php es como si llamara o todo ese fragmento de codigo del otro archivo
-// el requireonce funciona de la misma forma que el include sino q este impiden la carga de un mismo fichero varias veces, pero no da problema. 
+// el requireonce funciona de la misma forma que el include sino q este impiden la carga de un mismo fichero varias veces, pero no da problema.
 include 'conexion.php';
 $cedula = $_POST['cedulaI'];
+$codigoE =$_POST['codigoE'];
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
-$cargo = $_POST['cargo'];
 
 
 session_start();
@@ -19,12 +19,12 @@ session_start();
 $conexion = conectar();
 
 //la varaible sqlInsertar guarda la consulta que se quiera realizar, pero aun no la ejecuta ojo
-$sqlInsertar = "INSERT INTO RepresentanteEmpresa (cedulaPRep,codigoPro,codigoE, nombreR, apellidoR, correo, telefono) VALUES ('$cedula','$id','$codigoE', '$nombreR','$apellidoR', '$correo', '$telefono', '$cargo')"or die('No se realizo la consulta');
+$sqlInsertar = "INSERT INTO RepresentanteEmpresa (cedulaRep,codigoPro,codigoE, nombreR, apellidoR, correo, telefono)
+VALUES ('$cedula', $id, $codigoE, '$nombre','$apellido', '$correo', '$telefono')"or die('No se realizo la consulta');
 
-//la variable  resultado realiza la consulta con mysqli_query pasandole como entradas la variable conexion y la consulta, si marcha bien todo se ejecuta la consulta caso contrario pasa al error 
-$resultado = mysqli_query($conexion, $sqlInsertar) or die("Problemas al guardar los datos...  ");
+//la variable  resultado realiza la consulta con mysqli_query pasandole como entradas la variable conexion y la consulta, si marcha bien todo se ejecuta la consulta caso contrario pasa al error
+$resultado = mysqli_query($conexion,$sqlInsertar) or die("Problemas al guardar los datos...  ");
 
 cerrar($conexion);
 
-header("Location: plantilla.php?op=2");
-
+header("Location: plantilla.php?op=10");
